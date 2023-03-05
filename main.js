3ee172aed76bc616f77b1a58479f07c8757c2404
Natalia Buzhovych <tuzhay@gmail.com>
2023-03-05 16:00:13 +0200

    added sorting by gender

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ async function getNameFilms() {
 function NameFilms(arr) {
   document.querySelector("#buttons").innerHTML = "";
   arr.forEach((el) => {
-    createButton(el.episode_id).innerHTML = el.title;
+    createButton("#buttons", el.episode_id).innerHTML = el.title;
   });
   document.querySelectorAll("button").forEach((el) => {
     el.addEventListener("click", (e) => {
@@ -59,8 +59,9 @@ async function getPeople(num) {
       people[i] = await el[i].json();
     }
     spinner.setAttribute("hidden", "");
+    document.getElementById("characters").scrollIntoView();
 
-    if (a.hasChildNodes()) {
+    if (document.getElementById("characters").hasChildNodes()) {
       document.getElementById("characters").innerHTML = "";
     }
     creatListChacters(people);
@@ -69,9 +70,63 @@ async function getPeople(num) {
   }
 }
 
-function creatListChacters(arr, text) {
+function creatListChacters(arr) {
   let element = document.createElement("div");
+  createButton("#characters", "Male").innerHTML = "Only male";
+  createButton("#characters", "Female").innerHTML = "Only female";
+  createButton("#characters", "All").innerHTML = "ALL person";
+  document.querySelector(".Male").addEventListener("click", function () {
+    if (element.hasChildNodes()) {
+      element.innerHTML = "";
+    }
+    element.append(sortList(arr, "male"));
+  });
 
+  document.querySelector(".Female").addEventListener("click", function () {
+    if (element.hasChildNodes()) {
+      element.innerHTML = "";
+    }
+    element.append(sortList(arr, "female"));
+  });
+  document.querySelector(".All").addEventListener("click", function () {
+    if (element.hasChildNodes()) {
+      element.innerHTML = "";
+    }
+    element.append(creatListAll(arr));
+  });
+
+  element.append(creatListAll(arr));
+  document.querySelector("#characters").append(element);
+}
+function createCell(a, titleClass) {
+  const element = document.createElement("div");
+  element.appendChild(document.createTextNode(a));
+  element.classList.add(titleClass);
+  return element;
+}
+function sortList(arr, gender) {
+  let k = 1;
+  let sortElement = document.createElement("div");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].gender === gender) {
+      k = k + 1;
+      let box = document.createElement("div");
+      box.classList.add(k);
+      if (box.className % 2 == 0) {
+        box.style = "background:#5c5c27a8;display:flex";
+      } else {
+        box.style = "background:#454845a3;display:flex";
+      }
+      box.append(createCell(arr[i].name, "details"));
+      box.append(createCell(arr[i].birth_year, "details"));
+      box.append(createCell(arr[i].gender, "details"));
+      sortElement.append(box);
+    }
+  }
+  return sortElement;
+}
+function creatListAll(arr) {
+  let listAll = document.createElement("div");
   for (let i = 0; i < arr.length; i++) {
     let box = document.createElement("div");
     box.classList.add(i + 1);
@@ -83,23 +138,15 @@ function creatListChacters(arr, text) {
     box.append(createCell(arr[i].name, "details"));
     box.append(createCell(arr[i].birth_year, "details"));
     box.append(createCell(arr[i].gender, "details"));
-    element.append(box);
+    listAll.append(box);
   }
-  document.querySelector("#characters").append(element);
-}
-function createCell(a, titleClass) {
-  const element = document.createElement("div");
-  element.appendChild(document.createTextNode(a));
-  element.classList.add(titleClass);
-
-  return element;
+  return listAll;
 }
 
-function createButton(titleClass, html) {
+function createButton(nameClass, titleClass, html) {
   let button = document.createElement("button");
-  document.querySelector("#buttons").append(button);
+  document.querySelector(nameClass).append(button);
   button.classList.add(titleClass);
   button.innerHTML = html;
-
   return button;
 }
